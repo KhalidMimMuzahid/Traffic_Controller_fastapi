1926275eccd8740f0638399f3de387ac83bb278b
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-03-03 13:17:17 +0600

    implemented filtering

--- app/modules/zones/router.py
+++ app/modules/zones/router.py
@@ -18,7 +18,6 @@ async def add_zone(request: Request,zone: ZoneCreateRequest, db: AsyncSession = 
 )
 async def list_zones(page:int=1, limit:int=10, name:str=None, db: AsyncSession = Depends(get_db)):
     result= await get_zones(db, page, limit, name)
-    # return result
     # Call the helper function to create the response and return it, passing UserCreateResponse model
     return create_response(result["data"], ZoneListResponse, "Zones have retrieved successfully", result["meta_data"] )
     
